2d64fcca4b2c33767bd1631e7c05572fbf143d62
unsatiable <amadsen87@gmail.com>
2014-01-27 14:08:49 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectM::Application.config.secret_token = '314bfe85ba33417e72107ccca3b91e12495c16182caad7e26b000fd094aac536ac40068cc269315e059d81287b3eef1c0ec8bc09094469c59e05db64c2eb0851'
